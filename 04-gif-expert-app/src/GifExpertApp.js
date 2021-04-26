a62fbe035f6c46d6d69ae9f20bc607a48efe065c
Carlos Gutierrez <shades3002@gmail.com>
2021-04-26 19:38:39 -0400

    structure gif expert app

---
+++ 04-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+
+const GifExpertApp = () => {
+
+    return (
+        <Fragment>
+            <h2>GifExpertApp</h2>
+            <hr />
+        </Fragment>
+    );
+}
+
+export default GifExpertApp;
