fa6ef7f9dcb8331c2f2818384a36cdaf3f02f0fd
Carlos Gutierrez <shades3002@gmail.com>
2021-05-06 12:58:35 -0400

    function waitForNextUpdate

--- 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-expert-app/src/tests/hooks/useFetchGifs.test.js
@@ -4,14 +4,27 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Test hook useFetchGifs', () => {
     
-    test('should return init state', () => {
+    test('should return init state', async () => {
 
-        const { result } = renderHook(() => useFetchGifs( 'Goku' ) );
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs( 'Goku' ) );
         const { data, loading } = result.current;
-
+        await waitForNextUpdate();
         expect( data ).toEqual( [] );
         expect( loading ).toBe( true );
         
+    });
+    
+    test('should return a array the images and loading in false', async () => {
+        
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs( 'Goku' ) );
+
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBe( false );
+
     });
     
 });
