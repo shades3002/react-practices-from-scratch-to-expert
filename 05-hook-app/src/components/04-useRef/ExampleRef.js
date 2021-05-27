b5570a1060564c87e7ce828c83720d1514f33939
Carlos Gutierrez <shades3002@gmail.com>
2021-05-27 13:32:10 -0400

    useRef

---
+++ 05-hook-app/src/components/04-useRef/ExampleRef.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import '../02-useEffect/effects.css';
+import { MultipleCustomHooks } from '../03-examples/MultipleCustomHooks';
+
+export const ExampleRef = () => {
+
+
+    const [show, setShow] = useState(false)
+    
+    return (
+        <div>
+            <h1>Exmple Ref</h1>
+            <hr />
+
+            { show && <MultipleCustomHooks /> }
+
+            <button
+                className="btn btn-primary mt-5"
+                onClick={ () => {
+                    setShow( !show );
+                }}
+            >
+                Show/Hide
+            </button>
+            
+        </div>
+    )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { FocusScreen } from './components/04-useRef/FocusScreen';
-
+import { ExampleRef } from './components/04-useRef/ExampleRef';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HookApp } from './HookApp';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
 
 ReactDOM.render(
-  <FocusScreen />,
+  <ExampleRef />,
   document.getElementById('root')
 );
