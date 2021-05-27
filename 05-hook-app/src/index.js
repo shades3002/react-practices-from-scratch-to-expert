c3cfce619db7ad3e218aab0a28e40c86bfa7c63d
Carlos Gutierrez <shades3002@gmail.com>
2021-05-27 16:04:49 -0400

    Layout Effect Example

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ExampleRef } from './components/04-useRef/ExampleRef';
+
+import { LayoutEffectExample } from './components/05-useLayoutEffect/LayoutEffectExample';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -8,8 +9,10 @@ import { ExampleRef } from './components/04-useRef/ExampleRef';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import { ExampleRef } from './components/04-useRef/ExampleRef';
+
 
 ReactDOM.render(
-  <ExampleRef />,
+  <LayoutEffectExample />,
   document.getElementById('root')
 );
