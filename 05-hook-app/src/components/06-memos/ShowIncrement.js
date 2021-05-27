17b52fb9054f79b4c308dabd89dad8b25b9cb829
Carlos Gutierrez <shades3002@gmail.com>
2021-05-27 18:06:35 -0400

    useCallback

---
+++ 05-hook-app/src/components/06-memos/ShowIncrement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const ShowIncrement = ({ increment }) => {
+
+    console.log(' Me volvi a generar ');
+
+    return (
+        <button
+            className="btn btn-primary"
+            onClick={ () => {
+                increment(5)
+            }}
+        >
+            Increment
+        </button>
+    )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
+
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
@@ -11,8 +12,9 @@ import { MemoHook } from './components/06-memos/MemoHook';
 // import { ExampleRef } from './components/04-useRef/ExampleRef';
 // import { LayoutEffectExample } from './components/05-useLayoutEffect/LayoutEffectExample';
 // import { Memorize } from './components/06-memos/Memorize';
+// import { MemoHook } from './components/06-memos/MemoHook';
 
 ReactDOM.render(
-  <MemoHook />,
+  <CallbackHook />,
   document.getElementById('root')
 );
