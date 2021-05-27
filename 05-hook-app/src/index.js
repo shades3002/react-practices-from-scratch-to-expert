011a89c27d542c0422d900055b101b46e056dfae
Carlos Gutierrez <shades3002@gmail.com>
2021-05-27 12:19:35 -0400

    useFetch + CustomHook

--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
-//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
-//import { HookApp } from './HookApp';
+// import { HookApp } from './HookApp';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
 
 ReactDOM.render(
-  <FormWithCustomHook />,
+  <MultipleCustomHooks />,
   document.getElementById('root')
 );
